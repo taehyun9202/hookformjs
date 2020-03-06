2fda14e30c5fb2537deb3c879e28a5bc7bad0f4d
Tyler <taehyun9202@hotmail.com>
2020-03-06 10:50:14 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HookComponent from './components/HookComponent';
+
+function App() {
+  return (
+    <div>
+      <HookComponent  firstName = "" lastName = "" email = "" password = "" confirmPW = ""/>
+    </div>
+  )
+}
+
+export default App;
