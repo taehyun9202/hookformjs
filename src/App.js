19e6087dc82743e8ff67b91e54504da168df0654
taehyun9202 <60115779+taehyun9202@users.noreply.github.com>
2020-03-09 13:33:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 import HookComponent from './components/HookComponent';
+import BoxGenerator from './components/BoxGenerator';
 
 function App() {
   return (
     <div>
-      <HookComponent  firstName = "" lastName = "" email = "" password = "" confirmPW = ""/>
+      {/* <HookComponent/> */}
+      <BoxGenerator/>
     </div>
   )
 }
+
 
 export default App;
